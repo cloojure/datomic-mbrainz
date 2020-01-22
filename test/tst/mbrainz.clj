d5864c1dc08bb9d787046e2ec28de88a5cec4a18
Alan Thompson <cloojure@gmail.com>
2020-01-21 19:52:44 -0800

    misc

--- test/tst/mbrainz.clj
+++ test/tst/mbrainz.clj
@@ -86,6 +86,71 @@
   [& args]
   (query-map-impl args))
 
+; #todo change :find -> :return  ?
+(defn ^:no-doc query-map-cond-impl
+  [args]
+  (when-not (= :where (nth args 6))
+    (throw (IllegalArgumentException.
+             (str "find-base: 7th arg must be :where, received=" args))))
+  (let
+    [let-find-map  (apply hash-map (take 6 args))                             >> (spyx let-find-map)
+     where-vec     (td/where-clause (drop 7 args))                            >> (spyx-pretty where-vec)
+     preds-vec     (grab :preds let-find-map)                                 >> (spyx-pretty preds-vec)
+     preds2-vec    (mapv vector preds-vec)                                    >> (spyx-pretty preds2-vec)
+     let-vec       (grab :let let-find-map)                                   >> (spyx let-vec)
+     let-map       (apply hash-map let-vec)                                   >> (spyx let-map)
+     let-syms      (keys let-map)                                             >> (spyx let-syms)
+     let-srcs      (vals let-map)                                             >> (spyx let-srcs)
+     yield-vec     (grab :yield let-find-map)                                 >> (spyx yield-vec)
+     yield-kws     (mapv  query-sym->kw  yield-vec)                           >> (spyx yield-kws)
+     where-vec-final (glue where-vec preds2-vec)                           >> (spyx where-vec-final)
+     ]
+    `(let [
+           query-tuples# (d/q '{:find  ~yield-vec
+                                :in    [~@let-syms]
+                                :where ~where-vec-final
+                                }
+                           ~@let-srcs)
+           result-set#   (set (for [tuple# query-tuples#]
+                                (zipmap ~yield-kws (vec tuple#))))]
+       result-set#)))
+
+(defmacro query-map-cond
+  "Returns search results as a set of maps (i.e. a TupleSet, or #{ [s/Any] } in Prismatic Schema),
+   where each tuple is unique. Usage:
+
+    (td/query
+       :let    [$        (d/db *conn*)     ; assign multiple variables just like
+                ?name    \"Caribbean\"]    ;   in Clojure 'let' special form
+       :preds  [ (< 1960 ?year) (< ?year 1970) ]
+       :yield  [?e ?name]
+       :where  {:db/id ?eid  :person/name ?name  :location ?loc}
+               {:db/id ?eid  :weapon/type :weapon/wit} )
+
+  Unlike datomic.api/q, the query form does not need to be wrapped in a map literal nor is any
+  quoting required. Most importantly, the :in keyword has been replaced with the :let keyword, and
+  the syntax has been copied from the Clojure let special form so that both the query variables (the
+  variables $ and ?name in this case) are more closely aligned with their actual values. Also, the
+  implicit DB $ must be explicitly tied to its data source in all cases (as shown above).
+  The `:let` and `:yield` clauses may be in any order, but the `:where` clause must come last.
+
+  Each map in result set is keyword labeled such that:
+
+    (query-map
+      ...
+      :yield [?gid ?ident-type ?ident-gender]  ; Datomic query symbols like `?some-symbol`
+      ...)
+  produces output like:
+
+    #{...    ; maps keyed by keyword version `:some-symbol` with `?` stripped => `:`
+        {:gid           #uuid '76c9a186-75bd-436a-85c0-823e3efddb7f'
+         :ident-type    :artist.type/person
+         :ident-gender  :artist.gender/female}
+      ...)
+     "
+  [& args]
+  (query-map-cond-impl args))
+
 (dotest
   ; Testing the macro
   (when false
@@ -112,7 +177,7 @@
 
   (nl)
   (spyx-pretty :lennon-tracks-titles-partial
-    (take 20
+    (take 10
       (query-map
         :let [$ (live-db)
               ?artist-name "John Lennon"]
@@ -123,7 +188,7 @@
 
   (nl)
   (spyx-pretty :lennon-title-album-year
-    (take 20
+    (take 10
       (query-map
         :let [$ (live-db)
               ?artist-name "John Lennon"]
@@ -132,11 +197,42 @@
           {:db/id ?eid-artist :artist/name ?artist-name}
           {:db/id ?eid-track :track/artists ?eid-artist :track/name ?track-name}
           {:db/id ?eid-media :medium/tracks ?eid-track }
-          {:db/id ?eid-release :release/media ?eid-media :release/name ?release-name :release/year ?release-year }
-        )))
+          {:db/id ?eid-release :release/media ?eid-media :release/name ?release-name :release/year ?release-year } )
+      ))
 
 
 
+  (nl)
+  ; Testing the macro
+  (when false
+    (println "-----------------------------------------------------------------------------")
+    (pprint/pprint
+      (query-map-cond-impl
+       '[
+        :let [$ (live-db)
+                 ?artist-name "John Lennon"]
+        :yield [?track-name ?release-name ?release-year]
+        :preds [(< ?year 1970)]
+        :where
+        {:db/id ?eid-artist :artist/name ?artist-name}
+        {:db/id ?eid-track :track/artists ?eid-artist :track/name ?track-name}
+        {:db/id ?eid-media :medium/tracks ?eid-track}
+        {:db/id ?eid-release :release/media ?eid-media :release/name ?release-name :release/year ?release-year} ]) ))
+    )
+
+(dotest-focus
+  (spyx-pretty :lennon-title-album-year
+    (query-map-cond
+      :let [$ (live-db)
+            ?artist-name "John Lennon"]
+      :yield [?track-name ?release-name ?release-year]
+      :preds [(<= 1969 ?release-year) (<= ?release-year 1969) ]
+      :where
+      {:db/id ?eid-artist :artist/name ?artist-name}
+      {:db/id ?eid-track :track/artists ?eid-artist :track/name ?track-name}
+      {:db/id ?eid-media :medium/tracks ?eid-track}
+      {:db/id ?eid-release :release/media ?eid-media :release/name ?release-name :release/year ?release-year}
+      ))
   )
 
 
