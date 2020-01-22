2b8f4cfee5e4f9cbba1b857e6c65d2cc2f5bcf65
Alan Thompson <cloojure@gmail.com>
2020-01-22 07:16:28 -0800

    misc

--- test/tst/mbrainz.clj
+++ test/tst/mbrainz.clj
@@ -45,20 +45,76 @@
         odds  (mapv only (partition 1 2 (drop 1 seq-arg)))]
     [evens odds]))
 
+(s/defn query-sym? :- s/Bool
+  "Returns true iff a symbol is a legal Datomic query symbol (beginning with a `?` char)"
+  [arg :- s/Any]
+  (and (symbol? arg)
+    (= \? (first (name arg)))))
+
+(s/defn query-sym-wild? :- s/Bool
+  "Returns true iff a symbol is a legal Datomic query symbol (beginning with a `?` char),
+  and is a wildcard symbol (ending in a `*` char)"
+  [arg :- s/Any]
+  (and (query-sym? arg)
+    (= \* (last (name arg)))))
+
+(s/defn check-symbol-usage
+  "Throws an exception if:
+     - a wildcard query symbol is appears more than once
+     - any other symbol appears only once
+   Else returns `:ok` "
+  [syms-vec :- tsk/Vec]
+  (nl)
+  (let [syms-query-wild    (keep-if query-sym-wild? syms-vec)
+        syms-reg           (drop-if query-sym-wild? syms-vec)
+        freq-map-reg       (frequencies syms-reg)
+        freq-map-wild      (frequencies syms-query-wild)
+        orphan-syms        (vec (keys (submap-by-vals freq-map-reg [1] :missing-ok)))
+        val-over-1?        (fn [k v] (when (< 1 v)
+                                       [k v]))
+        overused-wildcards (vec (keys (keep-if val-over-1? freq-map-wild))) ]
+    (when (not-empty? orphan-syms)
+      (throw (ex-info "Orphan symbols found:" (vals->map orphan-syms))))
+    (when (not-empty? overused-wildcards)
+      (throw (ex-info "Overused wildcards found:" (vals->map overused-wildcards))))
+    :ok))
+
+(dotest
+  (is (query-sym? (quote ?name)))
+  (isnt (query-sym? (quote name)))
+  (isnt (query-sym? :name))
+  (isnt (query-sym? "name"))
+  (isnt (query-sym? 5))
+
+  (is (query-sym-wild? (quote ?name*)))
+  (isnt (query-sym-wild? (quote ?name)))
+  (isnt (query-sym-wild? :name*))
+  (isnt (query-sym-wild? "name*"))
+  (isnt (query-sym-wild? 5))
+
+  (is= :ok (check-symbol-usage '[aaa bbb bbb aaa]))
+  (throws? (check-symbol-usage '[aaa bbb aaa ]))
+  (is= :ok (check-symbol-usage '[?aaa* ?bbb ?bbb])) ; orphans must by query symbol AND wildcard to pass OK
+  (throws? (check-symbol-usage '[?aaa* ?aaa* bbb ]))
+  )
+
 ; #todo change :find -> :return  ?
 (s/defn ^:no-doc query-map-impl
   [ctx :- tsk/KeyMap]
   (let
-    [where-vec           (td/where-clause (grab :where ctx))               >> (spyx-pretty where-vec)
-     preds-vec           (get ctx :preds [])                               >> (spyx-pretty preds-vec)
-     preds2-vec          (mapv vector preds-vec)                           >> (spyx-pretty preds2-vec)
-     rules-vec           (get ctx :rules [])                               >> (spyx-pretty rules-vec)
-     let-vec             (grab :let ctx)                                   >> (spyx let-vec)
-     [let-syms let-srcs] (partition-even-odd let-vec)                      >> (spyx [let-syms let-srcs] )
-     yield-vec           (grab :yield ctx)                                 >> (spyx yield-vec)
-     yield-kws           (mapv  query-sym->kw  yield-vec)                  >> (spyx yield-kws)
-     where-vec-final     (glue where-vec preds2-vec rules-vec)             >> (spyx where-vec-final)
+    [where-vec            (td/where-clause (grab :where ctx))               >> (spyx-pretty where-vec)
+     preds-vec            (get ctx :preds [])                               >> (spyx-pretty preds-vec)
+     preds2-vec           (mapv vector preds-vec)                           >> (spyx-pretty preds2-vec)
+     rules-vec            (get ctx :rules [])                               >> (spyx-pretty rules-vec)
+     let-vec              (grab :let ctx)                                   >> (spyx let-vec)
+     [let-syms let-srcs]  (partition-even-odd let-vec)                      >> (spyx [let-syms let-srcs] )
+     yield-vec            (grab :yield ctx)                                 >> (spyx yield-vec)
+     yield-kws            (mapv  query-sym->kw  yield-vec)                  >> (spyx yield-kws)
+     where-vec-final      (glue where-vec preds2-vec rules-vec)             >> (spyx where-vec-final)
+     query-syms           (keep-if query-sym? (flatten [where-vec let-syms yield-vec rules-vec]))
      ]
+    (check-symbol-usage query-syms)
+    ; Look for orphaned symbols
     `(let [query-tuples# (d/q '{:find  ~yield-vec
                                 :in    [~@let-syms]
                                 :where ~where-vec-final }
@@ -104,6 +160,11 @@
   (query-map-impl ctx))
 
 (dotest
+  (let [ctr (atom 0)]
+    (doseq [it (d/datoms (live-db) :eavt)]
+      (swap! ctr inc))
+    (nl) (spy :total-datoms @ctr))
+
   (nl)
   (is= #{{:gid #uuid "76c9a186-75bd-436a-85c0-823e3efddb7f", :ident-type :artist.type/person, :ident-gender :artist.gender/female}}
     (query-map {:let   [$ (live-db)
@@ -193,8 +254,12 @@
                    (live-db) rules "John Lennon" ) ]
       (spyx-pretty result)))
 
+    )
+
+(dotest
+
   (when false
-    (println "#4-----------------------------------------------------------------------------")
+    (nl) (println "#4-----------------------------------------------------------------------------")
     (pprint/pprint
       (query-map-impl '{:let   [$ (live-db)
                                 % rules
@@ -207,32 +272,24 @@
                                 (<= ?release-year 1969)
                                 ]
                         :rules [(track-release ?eid-track ?eid-release)]})))
-  (nl)
-  (println "-----------------------------------------------------------------------------")
-  (println :lennon-with-rules)
-  (spyx-pretty
-    (query-map {:let   [$ (live-db)
-                        % rules
-                        ?artist-name "John Lennon"]
-                :yield [?track-name ?release-name ?release-year]
-                :where [{:db/id ?eid-artist :artist/name ?artist-name}
-                        {:db/id ?eid-track :track/artists ?eid-artist :track/name ?track-name}
-                        {:db/id ?eid-release :release/name ?release-name :release/year ?release-year}]
-                :preds [(<= 1969 ?release-year)
-                        (<= ?release-year 1969)]
-                :rules [(track-release ?eid-track ?eid-release)]
-                }))
+  (when true
+    (nl) (println "-----------------------------------------------------------------------------")
+    (println :lennon-with-rules)
+    (spyx-pretty
+      (query-map {:let   [$ (live-db)
+                          % rules
+                          ?artist-name "John Lennon"]
+                  :yield [?track-name ?release-name ?release-year]
+                  :where [{:db/id ?eid-artist :artist/name ?artist-name}
+                          {:db/id ?eid-track :track/artists ?eid-artist :track/name ?track-name}
+                          {:db/id ?eid-release :release/name ?release-name :release/year ?release-year}]
+                  :preds [(<= 1969 ?release-year)
+                          (<= ?release-year 1969)]
+                  :rules [(track-release ?eid-track ?eid-release)]})))
 
-  (nl)
-  (let [ctr (atom 0)]
-    (doseq [it (d/datoms (live-db) :eavt)]
-      (swap! ctr inc))
-    (spy :total-datoms @ctr))
+
 
   )
-
-
-
 
 
 
